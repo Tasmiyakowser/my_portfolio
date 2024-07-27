f89cfd367c563d1a1d56be23987da9d8f3a557a3
Tasmiyakowser <136718272+Tasmiyakowser@users.noreply.github.com>
2024-07-27 20:13:13 +0530

    Add files via upload

---
+++ css/main.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const typingText = "Computer Science Engineer";
+    let index = 0;
+    let currentText = '';
+  
+    function type
+  
